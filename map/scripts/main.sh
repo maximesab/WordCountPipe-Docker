353493126412c3c9180b82d1788795e17a59a271
maxime sabran <maxime.sabran@orange.com>
2016-08-11 00:08:53 +0000

    update 8-10-2016

---
+++ map/scripts/main.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+TMP_FILE=$(mktemp)
+echo "source ~/.bashrc" > $TMP_FILE
+echo ". /scripts/env_file" >> $TMP_FILE
+echo "rm -f $TMPFILE" >> $TMP_FILE
+echo "bash /scripts/inotify.sh" >>$TMP_FILE
+bash --rcfile $TMP_FILE
+
---
+++ master/rules.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 17:33:39 2016
+
+@author: maxime
+"""
+import manager
+
+
+
